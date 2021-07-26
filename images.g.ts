9fd485681eed69032e0b247c5b4ddef90dc41a5b
G184692021 <86590046+G184692021@users.noreply.github.com>
2021-07-26 17:43:54 +0900

    飛行機が可愛くなかったのでカービィのゲームを作りました。

---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "bogy":return img`
+. f f f . . . . . . . . . f f f 
+. f 4 f f f . . . f f f f f 4 f 
+. f 4 4 4 4 f f f 4 4 4 4 4 4 f 
+. . f 4 4 4 4 4 4 4 4 4 4 f f . 
+. . f 4 4 4 4 4 4 4 4 4 f f f . 
+. f 4 4 f 4 4 f 4 4 4 4 4 f . . 
+. f 4 4 f 4 4 f 4 4 4 4 4 f . . 
+f 4 4 4 f 4 4 f 4 4 4 4 4 f . . 
+f d d 4 4 4 4 4 4 d d 4 4 f . . 
+f 4 d 4 f f f f 4 d d 4 4 f . . 
+f 4 4 4 f d d f 4 4 4 4 4 f . . 
+f 4 4 4 f d d f 4 4 4 4 4 f . . 
+. f 4 4 4 f f 4 4 4 4 4 f f . . 
+. . f 4 4 4 4 4 4 4 4 4 f . . . 
+. . . f f 4 4 4 4 f f f . . . . 
+. . . . f f f f f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
